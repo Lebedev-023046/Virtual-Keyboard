adc62628ed90accd77183e00bd2810b7f130f80a
lebedev-023046 <lebedev.023046@gmail.com>
2022-05-07 18:42:12 +0300

    feat: add visual keyboard

---
+++ virtual-keyboard/js/index.js
@@ -0,0 +1,144 @@
+const body = document.querySelector('body')
+
+const container = document.createElement('div')
+container.classList.add('container')
+
+const title = document.createElement('div')
+title.innerHTML = '<h1>Virtual Keyboard</h1>'
+title.classList.add('title')
+body.appendChild(title)
+
+const textArea = document.createElement('textarea')
+textArea.classList.add('textarea')
+textArea.setAttribute('keyss', '5')
+textArea.setAttribute('columns', '45')
+textArea.setAttribute('autofocus', 'true')
+container.appendChild(textArea)
+
+const keyboard = document.createElement('div')
+keyboard.classList.add('keyboard')
+container.appendChild(keyboard)
+
+const keys = document.createElement('div')
+keys.classList.add('key-block')
+keyboard.appendChild(keys)
+
+for (let i=0;i<64;i++) {
+    if (i === 13) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('backspace')
+    }
+    else if (i === 14) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('tab')
+    }
+    else if (i === 28) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('del')
+    }
+    else if (i === 29) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('caps')
+    }
+    else if (i === 29) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('caps')
+    }
+    else if (i === 41) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('enter')
+    }
+    else if (i === 42) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('shift-l')
+    }
+    else if (i === 53) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('arr-t')
+    }
+    else if (i === 54) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('shift-r')
+    }
+    else if (i === 55) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('ctrl')
+    }
+    else if (i === 56) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('win')
+    }
+    else if (i === 57) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('alt')
+    }
+    else if (i === 58) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('space')
+    }
+    else if (i === 59) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('alt')
+    }
+    else if (i === 60) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('arr-l')
+    }
+    else if (i === 61) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('arr-b')
+    }
+    else if (i === 62) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('arr-r')
+    }
+    else if (i === 63) {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+        key.classList.add('ctrl')
+    }
+    else {
+        let key = document.createElement('span')
+        keys.appendChild(key)
+        key.classList.add('key')
+    }
+}
+
+
+
+body.appendChild(container)
